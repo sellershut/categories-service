1fa9e5b7edf05456334ae2f5a2a0ab43e32bd1c6
rtkay123 <dev@kanjala.com>
2025-02-16 11:02:02 +0200

    feat: grpc server template

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+pub mod server;
+pub mod state;
+
+use serde::Deserialize;
+use state::AppState;
+use tracing::{debug, trace};
+
+#[derive(Deserialize)]
+pub struct AppConfig {
+    something: String,
+}
+
+pub async fn run(state: AppState, tx: tokio::sync::oneshot::Sender<u16>) -> anyhow::Result<()> {
+    trace!("running migrations");
+    sqlx::migrate!("./migrations")
+        .run(&state.services.postgres)
+        .await?;
+    debug!("ran migrations");
+
+    server::serve(state, tx).await
+}
